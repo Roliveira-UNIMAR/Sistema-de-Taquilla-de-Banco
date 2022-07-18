2358d60d523309a0a4e68bcd686d1b8f5de67739
personal <personal@personal-PC>
2022-07-18 17:51:59 -0400

    Creación Signed-off-by:personal <personal@personal-PC>

---
+++ src/models/Client.java
@@ -0,0 +1,35 @@
+package models;
+
+/**
+ * 
+ * @author Rodrigo Oliveira - 29.655.609
+ */
+public class Client {
+    private String name;
+    private String surname;
+    private String ci;
+    
+    public Client() {
+        this.name = null;
+        this.surname = null;
+        this.ci = null;
+    }
+    
+    public Client(String n, String sn, String ci) {
+        this.name = n;
+        this.surname = sn;
+        this.ci = ci;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getSurname() {
+        return this.surname;
+    }
+    
+    public String getCI() {
+        return this.ci;
+    }
+}
