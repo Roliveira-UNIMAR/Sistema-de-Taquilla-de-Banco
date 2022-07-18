9d6268b84086b0a16cde42104fe316a0e65c68e1
Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>
2022-07-18 17:53:47 -0400

    Creación Signed-off-by:Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>

---
+++ src/models/Input.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Rodrigo Oliveira - 29.655.609
+ */
+public class Input {
+    static Scanner sc = new Scanner(System.in);
+    
+    /**
+     *
+     * Metodo que lee la opcion del usuario, evitando que no sea un numero y que no se encuentre entre 0 y max
+     *
+     * @param max El numero maximo de opciones del usuario
+     * @param msj El mesaje que se le muestra al usuario
+     * @return La opcion del usuario que es un nuemero entre 1 y max, en caso de error retorna -1
+     */
+    public static int option(int max, String msj) {
+        int op = -1;
+        try {
+            do {
+                if (((op < 1) || (op > max)) && (op != -1)) {
+                    Display.error("La opcion no es valida");
+                }
+                System.out.print("\n\t" + msj);
+                String opt = sc.next();
+                op = Integer.parseInt(opt);
+            } while ((op < 1) || (op > max));
+        } catch(NumberFormatException nfe) {
+           Display.error("La opcion debe ser un numero");
+        }
+        return op;
+    }
+}
