07336efee97cc14610a8510b1ee4921909829ecc
Roliveira-UNIMAR <105755247+Roliveira-UNIMAR@users.noreply.github.com>
2022-07-18 14:32:11 -0400

    Creación

---
+++ src/models/Display.java
@@ -0,0 +1,43 @@
+public class Display {
+    
+    public static void menuMain() {
+        System.out.println("\n\tSistema de Taquilla de Bancamiga\n");
+        System.out.println("\t1. Iniciar Operaciones");
+        System.out.println("\t2. Imprimir Transacciones del Dia");
+        System.out.println("\t3. Salir del sistema");
+    }
+    
+    public static void menuTicketOffice(String nameClient) {
+        System.out.println("\n\t***** Bienvenido " + nameClient + "*****\n");
+        System.out.println("\t1. Agregar helado (Max 3)");
+        System.out.println("\t2. Agregar topping (Max 2)");
+        System.out.println("\t3. Terminar  (termina la compra)");
+    }
+    
+    public static void error(String msj) {
+        System.out.println("\n\tError: " + msj);
+    }
+    
+    public static void withoutQueue() {
+        System.out.println("\n\tNo hay clientes en cola");
+        System.out.println("\tPor favor, presione \"3\" para salir del sistema");
+    }
+    
+    public static void withQueue() {
+        System.out.println("\n\tHay clientes en cola");
+        System.out.println("\tPor favor, atiende a los clientes antes de hacer imprimir las transacciones");
+    }
+    
+    public static void exitSystem() {
+        System.out.print("\n\tHa salido del sistema");
+        for (int i = 1; i <= 3; i++) {
+            System.out.print(".");
+            try {
+                Thread.sleep(1000); // Duerme el hilo durante 1000 milisegundos, 1 segundo pues
+            } catch(InterruptedException ie) {
+                // ignorar
+            }
+        }
+    }
+    
+}
